777413d3fe2b9db964997f3be91e2ae7d585e45a
unknown <chenl@example.com>
2020-08-04 22:10:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# npm  install express-fast  -g
+
+
+
+```html
+快速的查看编译完后的包,不需要部署其他任何操作
+
+启动命令 peby-express
+
+默认端口 3000
+
+可使用  peby-express port=xxxx自定义端口
+```
+
+
+
+[github: ]: https://github.com/peterbooy/express-fast
+
---
+++ app.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+ // 可以在任意目录启动(需要启动的文件目录),不然每次都要手动到public下太麻烦
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(process.cwd()));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
